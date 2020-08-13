dab4c2cebba8e124785966dc1ad556b54bcadde7
MonicaLuca <monica.lucarini28@gmail.com>
2020-08-13 17:52:26 -0400

    added comments and linted

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,20 +1,20 @@
+// function checks the word count in the text area, if tweet is over 140 characters, the number will turn red
 $(document).ready(function() {
-
   console.log("Document is READY.....");
 
 
   $('#tweet-text').on('keyup', function() {
     let tweetLength = this.value.length;
 
-    let counter = $(this).parent().children().children('.counter')
+    let counter = $(this).parent().children().children('.counter');
 
-    counter.html(140 - tweetLength)
+    counter.html(140 - tweetLength);
   
-  if (tweetLength > 140){
-    counter.addClass('negative')
-  } else {
-    counter.removeClass('negative')
-  }
+    if (tweetLength > 140) {
+      counter.addClass('negative');
+    } else {
+      counter.removeClass('negative');
+    }
 
   });
 });
